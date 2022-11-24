f9ceb50c661adda8bba74c4e480a142ca26c48ee
NXTID <117934144+NXTID@users.noreply.github.com>
2022-11-24 19:42:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void serie(int a){
+    int risultato=1;
+    int precedente=1;
+    int x=0;
+
+    cout<<risultato<<endl;
+    cout<<risultato<<endl;
+
+    for(int i=(a-2); i>0; i--){
+        x=risultato;
+        risultato= risultato+precedente;
+        cout<<risultato<<endl;
+        precedente=x;
+    }
+}
 int main() {
-   return 0;
+    int n=0;
+
+    while(n<2){
+        cin>>n;
+
+        if(n<2)
+            cout<<"errore"<<endl;
+        else if(n>=2){
+            serie(n);
+            break;
+        }
+    }
+    return 0;
 }
